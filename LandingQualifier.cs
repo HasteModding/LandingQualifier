516d64e0fe1be46b688cf04cbee381b92210aa71
Alice <alice.is.cute@proton.me>
2025-04-15 01:41:30 +0300

    factor out logging refactor how vanilla strings (or ones modified by other mods) are gotten "NoVanilla" landing precision fixes are equivalent to non "No Vanilla" ones right now disable unimplemented settings, remove obsolete settings Extra Landing Qualities will still override other mods' modifications to label

--- LandingQualifier.cs
+++ LandingQualifier.cs
@@ -1,3 +1,5 @@
+// ReSharper disable MemberCanBePrivate.Global
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using JetBrains.Annotations;
@@ -7,106 +9,109 @@ using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Localization;
+using Zorro.Core.CLI;
 using Zorro.Settings;
+using Debug = UnityEngine.Debug;
 
 namespace dev.alice_is.LandingQualifier;
 
 [LandfallPlugin]
 [SuppressMessage("ReSharper", "RedundantDefaultMemberInitializer")]
-public class LandingQualifier {
+public static class LandingQualifier {
 
-	public static float                     lastAngle              = -1;
-	public static float                     lastScore              = -1;
-	public static ScoreStyle                style                  = ScoreStyle.Vanilla;
-	public static bool                      extras                 = false;
-	public static bool                      invertLandingPrecision = false;
-	public static bool                      overrideThresholds     = false;
-	public static LandingPrecisionFixState  landingPrecisionFix    = LandingPrecisionFixState.Off;
-	
+	public static ScoreStyle                style               = ScoreStyle.Vanilla;
+	public static bool                      extras              = false;
+	public static bool                      moreLandingsCompat  = false;
+	public static bool                      overrideThresholds  = false;
+	public static LandingPrecisionFixState  landingPrecisionFix = LandingPrecisionFixState.Vanilla;
+
+	public static void log(object message) {
+		switch (message) {
+		case Exception:
+			Debug.Log("[Landing Qualifier]");
+			Debug.Log(message);
+		break;
+		default:
+			Debug.Log($"[Landing Qualifier] {message}");
+		break;
+		}
+	}
+
 	static LandingQualifier() {
 		On.PlayerMovement.GetLanding += (prev, movement, hit) => {
 			var res = prev.Invoke(movement, hit)!;
 			var rig = movement.GetComponent<Rigidbody>();
-			if (landingPrecisionFix != LandingPrecisionFixState.Off) fixLanding(res, hit);
-			lastAngle = Vector3.Angle(rig.velocity, hit.normal);
-			lastScore = (float)res.GetType().GetField("landingScore").GetValue(res);
+			if (landingPrecisionFix != LandingPrecisionFixState.Vanilla) fixLanding(res, hit);
+			last.angle        = Vector3.Angle(rig.velocity, hit.normal);
+			last.landingScore = (float)res.GetType().GetField("landingScore").GetValue(res);
 			return res;
 		};
 
-		var flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+		const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
 		
 		On.UI_LandingScore.Land += (prev, _this, landingType, saved) => {
-			if (style == ScoreStyle.Vanilla && !extras) {
-				prev(_this, landingType, saved);
-				return;
-			}
-			string str;
-			if (extras && lastScore > 0.9995) {
-				str = "Impeccable!!";
-			} else if (extras && lastScore > 0.9945) {
-				str = "Pure Perfect!";
-			} else if (!saved) {
-				str = (_this.GetType().GetField(landingType switch {
-					LandingType.Bad     => "m_badLanding",
-					LandingType.Ok      => "m_okLanding",
-					LandingType.Good    => "m_goodLanding",
-					LandingType.Perfect => "m_perfectLanding",
-					_                   => throw new ArgumentOutOfRangeException(nameof(landingType), landingType, null)
-				}, flags)!.GetValue(_this) as LocalizedString)!.GetLocalizedString();
-			} else str = $"\"{(_this.GetType().GetField("m_perfectLanding", flags)!.GetValue(_this) as LocalizedString)!.GetLocalizedString()}\"";
-			str += style switch {
-				ScoreStyle.Angle => $" ({Math.Abs(90 - lastAngle):f1}°)",
-				ScoreStyle.Score => $" ({lastScore * 100:f1}%)",
-				_ => $" ({Math.Abs(90 - lastAngle):f1}°, {lastScore * 100:f1}%)"
+			prev(_this, landingType, saved);
+			if (style == ScoreStyle.Vanilla && !extras) return;
+			var text = (_this.GetType().GetField("text", flags)!.GetValue(_this) as TextMeshProUGUI)!;
+			var len = text.text.Length;
+			var str = extras switch {
+				true when last.landingScore > 0.9995 => "Impeccable!!",
+				true when last.landingScore > 0.9945 => "Pure Perfect!",
+				_                            => text.text
+			} + style switch {
+				ScoreStyle.Angle => $" ({Math.Abs(90 - last.angle):f1}°)",
+				ScoreStyle.Score => $" ({last.landingScore * 100:f1}%)",
+				_ => $" ({Math.Abs(90 - last.angle):f1}°, {last.landingScore * 100:f1}%)"
 			};
-			/*str += $"""
-			{last.t}
-			{last.x}
-			{last.speedBefore}
-			{last.speedAfterBefore}
-			{last.speedAfterAfter}
-			""";*/
-			(_this.GetType().GetField("text", flags)!.GetValue(_this) as TextMeshProUGUI)!.text = str;
-			// prev(_this, LandingType.None, false);
-			_this.GetComponent<Animator>().speed = 0.5f;
-			if(landingType != LandingType.None) _this.GetComponent<Animator>().Play(landingType switch {
-				LandingType.Bad     => "An_UI_Bad",
-				LandingType.Ok      => "An_UI_OK",
-				LandingType.Good    => "An_UI_Good",
-				LandingType.Perfect => "An_UI_Perfect",
-				_                   => ""
-			}, 0, 0);
+			text.text = str;
+			_this.GetComponent<Animator>().speed *= 1.0f * len / str.Length;
 		};
 	}
 	
-	struct save {
+	[SuppressMessage("ReSharper", "NotAccessedField.Local")]
+	public record struct Save {
 		public float x;
 		public float t;
+		public float angle;
 		public float speedBefore;
 		public float speedAfterBefore;
 		public float speedAfterAfter;
+		public float landingScore;
 	}
 
-	private static save last;
+	[SuppressMessage("ReSharper", "NotAccessedField.Local")]
+	private static Save last;
 	private static void fixLanding(object landing, RaycastHit hit) {
 		var velBefore = (Vector3)landing.GetType().GetField("velBefore")!.GetValue(landing)!;
 		var speedBefore = velBefore.magnitude;
-		var a = invertLandingPrecision? (0.65f, 1f): (1f, 0.65f);
-		var t = Mathf.Lerp(a.Item1, a.Item2, GameDifficulty.currentDif.landingPresicion);
+		var t = Mathf.Lerp(0.65f, 1, GameDifficulty.currentDif.landingPresicion);
 		var velAfter = Vector3.ProjectOnPlane(velBefore, hit.normal);
 		var x = t + speedBefore * (1 - t) / velAfter.magnitude;
-		if(landingPrecisionFix == LandingPrecisionFixState.Speed) velAfter.Scale(new(x, x, x));
-		landing.GetType().GetField("velAfter")!.SetValue(landing, velAfter);
+		if(landingPrecisionFix is LandingPrecisionFixState.Speed or LandingPrecisionFixState.SpeedNoVanilla) velAfter.Scale(new(x, x, x));
 		var speedAfter        = velAfter.magnitude;
-		last                  = new();
-		last.t                = t;
-		last.x                = x;
-		last.speedBefore      = speedBefore;
-		last.speedAfterBefore = (float)landing.GetType().GetField("speedAfter")!.GetValue(landing);
-		last.speedAfterAfter  = speedAfter;
-		var landingScore      = speedAfter * (landingPrecisionFix == LandingPrecisionFixState.Score ? x : 1) / speedBefore;
+		var landingScore      = speedAfter * (landingPrecisionFix is LandingPrecisionFixState.Score or LandingPrecisionFixState.ScoreNoVanilla ? x : 1) / speedBefore;
+		last                  = new Save {
+			t = t,
+			x = x,
+			speedBefore = speedBefore,
+			speedAfterBefore = (float)landing.GetType().GetField("speedAfter")!.GetValue(landing),
+			speedAfterAfter = speedAfter,
+		};
+		landing.GetType().GetField("velAfter")!.SetValue(landing, velAfter);
 		landing.GetType().GetField("speedAfter")!.SetValue(landing, speedAfter);
 		landing.GetType().GetField("landingScore")!.SetValue(landing, landingScore);
+	}
+	
+	// [ConsoleCommand]
+	[UsedImplicitly]
+	public static void tryLoad(string Class) {
+		log(Class);
+		try {
+			var loaded = Type.GetType(Class);
+			log(loaded!.Name);
+		} catch (Exception e) {
+			log(e);
+		}
 	}
 }
 
@@ -140,10 +145,11 @@ public class StyleSetting: EnumSetting<ScoreStyle>, IExposedSetting {
 }
 
 public enum LandingPrecisionFixState {
-	Off,
+	Vanilla,
 	Score,
-	[UsedImplicitly]
 	Speed,
+	ScoreNoVanilla,
+	SpeedNoVanilla,
 }
 
 [HasteSetting]
@@ -154,16 +160,18 @@ public class LandingPrecisionFix: EnumSetting<LandingPrecisionFixState>, IExpose
 	}
 
 	public string GetCategory() => "lQ";
-	protected override LandingPrecisionFixState GetDefaultValue() => LandingPrecisionFixState.Off;
+	protected override LandingPrecisionFixState GetDefaultValue() => LandingPrecisionFixState.Vanilla;
 	public LocalizedString GetDisplayName() => new UnlocalizedString("Landing Precision Difficulty Fix");
 		
 	public override List<LocalizedString> GetLocalizedChoices() => [
-		new UnlocalizedString("Off"),
+		new UnlocalizedString("Vanilla"),
 		new UnlocalizedString("Score Only"),
 		new UnlocalizedString("Speed and Score"),
+		new UnlocalizedString("Score Only, No vanilla"),
+		new UnlocalizedString("Speed and Score, No vanilla"),
 	];
 }
-	
+
 [HasteSetting]
 [UsedImplicitly]
 public class EnableExtraQualities: OffOnSetting, IExposedSetting {
@@ -180,8 +188,8 @@ public class EnableExtraQualities: OffOnSetting, IExposedSetting {
 		new UnlocalizedString("On"),
 	];
 }
-	
-[HasteSetting]
+
+// [HasteSetting]
 [UsedImplicitly]
 public class EnableThresholdOverrides: OffOnSetting, IExposedSetting {
 	public override void ApplyValue() {
@@ -197,26 +205,9 @@ public class EnableThresholdOverrides: OffOnSetting, IExposedSetting {
 		new UnlocalizedString("On"),
 	];
 }
-	
-[HasteSetting]
+
+// [HasteSetting]
 [UsedImplicitly]
-public class InvertLandingPrecision: OffOnSetting, IExposedSetting {
-	public override void ApplyValue() {
-		LandingQualifier.invertLandingPrecision = Value == OffOnMode.ON;
-	}
-
-	public string GetCategory() => "lQ";
-	protected override OffOnMode GetDefaultValue() => OffOnMode.ON;
-	public LocalizedString GetDisplayName() => new UnlocalizedString("Invert Landing Precision Setting (so that 0% is easier than 100%)");
-		
-	public override List<LocalizedString> GetLocalizedChoices() => [
-		new UnlocalizedString("Off"),
-		new UnlocalizedString("On"),
-	];
-}
-
-	
-[HasteSetting]
 public class PerfectThreshold: FloatSetting, IExposedSetting {
 	public static Fact easyPerfectsUsed = new("runUsedEasyPerfects");
 	public override void ApplyValue() {
@@ -227,6 +218,23 @@ public class PerfectThreshold: FloatSetting, IExposedSetting {
 	protected override float GetDefaultValue() => 0.95f;
 	protected override float2 GetMinMaxValue() => new(0, 1);
 	public LocalizedString GetDisplayName() => new UnlocalizedString("Score Style");
+}
+	
+// [HasteSetting]
+[UsedImplicitly]
+public class IngameStats: OffOnSetting, IExposedSetting {
+	public override void ApplyValue() {
+		LandingQualifier.moreLandingsCompat = Value == OffOnMode.ON;
+	}
+
+	public string GetCategory() => "lQ";
+	protected override OffOnMode GetDefaultValue() => OffOnMode.ON;
+	public LocalizedString GetDisplayName() => new UnlocalizedString("Enable More Landings Compatibility");
+		
+	public override List<LocalizedString> GetLocalizedChoices() => [
+		new UnlocalizedString("Off"),
+		new UnlocalizedString("On"),
+	];
 }
 
 
